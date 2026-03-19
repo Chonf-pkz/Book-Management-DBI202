92e750803c40a13b2177c8d9ef8cfcc08cf50658
phutan24072006-ui <phutan24072006@gmail.com>
2026-03-19 20:43:58 +0700

    Update 5caujoinandgroupby.sql

--- SQL Commands/5caujoinandgroupby.sql
+++ SQL Commands/5caujoinandgroupby.sql
@@ -1,37 +1,40 @@
--- Số lượng sách theo từng nhà xuất bản
-SELECT [p].[publisher_name], COUNT([b].[book_id]) AS [total_books]
-FROM [dbo].[publisher] AS [p]
-INNER JOIN [dbo].[book] AS [b] ON [p].[publisher_id] = [b].[publisher_id]
-GROUP BY [p].[publisher_name];
-GO
+-- 1. Thống kê tổng số lượng sách mà mỗi tác giả đã viết
+CREATE VIEW v_TotalBooksByAuthor AS
+SELECT a.author_name, COUNT(ba.book_id) AS total_books
+FROM author a
+JOIN book_of_author ba ON a.author_id = ba.author_id
+GROUP BY a.author_id, a.author_name;
 
--- Tổng tiền phạt của từng thành viên
-SELECT [m].[name], ISNULL(SUM([ld].[sum_of_fine]), 0) AS [total_fine_amount]
-FROM [dbo].[member] AS [m]
-INNER JOIN [dbo].[loan] AS [l] ON [m].[member_id] = [l].[member_id]
-INNER JOIN [dbo].[loan_detail] AS [ld] ON [l].[loan_id] = [ld].[loan_id]
-GROUP BY [m].[member_id], [m].[name];
-GO
 
--- Số lượng bản sao sách theo từng thể loại
-SELECT [c].[category_name], COUNT([bcop].[book_copy_id]) AS [total_copies]
-FROM [dbo].[category] AS [c]
-INNER JOIN [dbo].[book_category] AS [bc] ON [c].[category_id] = [bc].[category_id]
-INNER JOIN [dbo].[book_copy] AS [bcop] ON [bc].[book_id] = [bcop].[book_id]
-GROUP BY [c].[category_name];
-GO
+-- 2. Thống kê tổng số lần mượn sách của từng thành viên
+CREATE VIEW v_MemberLoanCount AS
+SELECT m.name, COUNT(l.loan_id) AS total_loans
+FROM member m
+JOIN loan l ON m.member_id = l.member_id
+GROUP BY m.member_id, m.name;
 
--- Số lượt mượn của từng cuốn sách
-SELECT [b].[name_book], COUNT([ld].[loan_id]) AS [times_borrowed]
-FROM [dbo].[book] AS [b]
-INNER JOIN [dbo].[book_copy] AS [bc] ON [b].[book_id] = [bc].[book_id]
-INNER JOIN [dbo].[loan_detail] AS [ld] ON [bc].[book_copy_id] = [ld].[book_copy_id]
-GROUP BY [b].[book_id], [b].[name_book];
-GO
+-- 3. Thống kê tổng tiền phạt thu được theo từng chính sách
+CREATE VIEW v_TotalFineByPolicy AS
+SELECT p.policy_id, SUM(ld.sum_of_fine) AS total_fine
+FROM policy_rule p
+JOIN loan l ON p.policy_id = l.policy_id
+JOIN loan_detail ld ON l.loan_id = ld.loan_id
+GROUP BY p.policy_id;
 
--- Số lượng sách do mỗi thủ thư quản lý
-SELECT [lib].[librarian_name], COUNT([b].[book_id]) AS [managed_books]
-FROM [dbo].[librarian] AS [lib]
-INNER JOIN [dbo].[book] AS [b] ON [lib].[librarian_id] = [b].[librarian_id]
-GROUP BY [lib].[librarian_id], [lib].[librarian_name];
-GO
+
+-- 4. Thống kê tổng số lượng bản sao hiện có của mỗi đầu sách
+CREATE VIEW v_BookCopyCount AS
+SELECT b.name_book, COUNT(bc.book_copy_id) AS total_copies
+FROM book b
+JOIN book_copy bc ON b.book_id = bc.book_id
+GROUP BY b.book_id, b.name_book;
+
+
+-- 5. Thống kê số lượng sách chưa trả (đang mượn) của từng thành viên
+CREATE VIEW v_UnreturnedBooksByMember AS
+SELECT m.name, COUNT(ld.book_copy_id) AS unreturned_books
+FROM member m
+JOIN loan l ON m.member_id = l.member_id
+JOIN loan_detail ld ON l.loan_id = ld.loan_id
+WHERE ld.return_date IS NULL
+GROUP BY m.member_id, m.name;
